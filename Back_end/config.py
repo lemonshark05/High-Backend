66da3da3e756ed8dbfd3ad9acd9c56b897d09fdb
zhimin meng <mengzhimin5@gmail.com>
2023-07-12 16:09:20 -0700

    #14 git commit -m "Removed client_secret.json from git tracking"

--- Back_end/config.py
+++
@@ -1,7 +0,0 @@
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@localhost:5433/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GOOGLE_OAUTH_CLIENT_ID="339140115579-73sorcnffv0bmdajkej4mrg4efqu7h4g.apps.googleusercontent.com"  # Replace with your Client ID
-    GOOGLE_OAUTH_CLIENT_SECRET="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"  # Replace with your Client Secret
